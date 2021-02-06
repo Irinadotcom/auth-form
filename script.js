ff360b70dc34b45adb8afddc7724b30dbd19c1a8
Irinadotcom <irinagri.work@gmail.com>
2021-02-06 12:40:23 +0200

    add basic js

---
+++ script.js
@@ -0,0 +1,28 @@
+const g = 9.8;
+const pi = 3.15;
+
+
+let password = "";
+let username = "";
+
+// function f(x) {}
+
+// const f = (x) => {
+//     return x * x;
+// }
+
+
+
+// let x = prompt("Enter x", '');
+// alert(x);
+
+// let result =f(x);
+// console(result);
+
+// $(document).ready(function {
+//     console.log('Document is ready');
+// })
+
+const login = () => {
+    console.log('on press');
+}
